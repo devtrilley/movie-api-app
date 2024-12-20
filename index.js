4822ed9424c313c0815d40a6da85e95101f19bc3
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-19 20:58:18 -0500

    Created search route, made it's handler async, used try-catch to fetch with Axios.

--- index.js
+++ index.js
@@ -15,6 +15,34 @@ const PORT = 3000;
 // This line is know as "Middleware", code that runs between the request and response
 app.use(express.json()); // the .json() method parses JSON to JS code
 
+// Sets route to search for movies
+app.get("/search", async (req, res) => {
+  const query = req.query.query; // Gets query param from our URL
+
+  // If there is no query added to the URL. Also adds query validation for empty queries and searches too long for realistic searches
+  // .trim() was used in cases where whitespace was added unneedingly
+  if (!query || query.trim() === '' || query.length > 100) {
+    return res.status(400).json({ error: "You must add a query parameter!" });
+  }
+
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/search/movie",
+      {
+        params: {
+          api_key: process.env.MOVIEDB_API_KEY, // .env file API key property
+          query: query, // The users search query
+        },
+      }
+    );
+
+    res.json(response.data.results);
+  } catch (error) {
+    console.log(`Error fetching movies: ${error.message}`);
+    res.status(500).json({ error: "Failed to fetch movies" });
+  }
+});
+
 // Starts the app to listen
 app.listen(PORT, () => {
   console.log(`Server is live! At http://localhost:${PORT}`);
